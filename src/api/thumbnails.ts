1f4002ff5f53ff83016f2e63f168e37b57b31188
TheRealStingos <justmo10@gmail.com>
2026-06-19 16:57:33 -0400

    add thumbnail data encode to base64

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,16 +1,16 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { buffer } from "stream/consumers";
 
 type Thumbnail = {
   data: ArrayBuffer;
   mediaType: string;
 };
 
-const videoThumbnails: Map<string, Thumbnail> = new Map();
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -22,18 +22,6 @@ export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
   if (!video) {
     throw new NotFoundError("Couldn't find video");
   }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
 }
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
@@ -44,10 +32,38 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
+  const formData = await req.formData()
+  const image = formData.get("thumbnail")
+  if (!(image instanceof File)) {
+    throw new BadRequestError("Invalild File")
+  }
+
+  const MAX_UPLOAD_SIZE = 10 << 20;
+  if (image.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File too large")
+  }
+
+  const type = image.type
+  const data = await image.arrayBuffer();
+
+  const video = getVideo(cfg.db, videoId);
+  if (video?.userID != userID) {
+    throw new UserForbiddenError("You do not have permission");
+  }
+
+
+  const baseData = Buffer.from(data).toString("base64")
+
+  const dataUrl = `data:${type};base64,${baseData}`
+
+
+  video.thumbnailURL = dataUrl
+  updateVideo(cfg.db, video);
+
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   // TODO: implement the upload here
 
-  return respondWithJSON(200, null);
+  return respondWithJSON(200, video);
 }
